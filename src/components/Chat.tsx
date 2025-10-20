79ef6031d3b9fd409c5cf592bfbe04c1e084f812
CreateWebGit <hej@createweb.se>
2025-10-20 19:56:06 +0200

    made api connections for ai and inputs

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -3,25 +3,79 @@
 import { useState, useRef, FormEvent } from "react"
 import { ArrowUpIcon } from "@phosphor-icons/react/dist/ssr";
 
+import hljs from 'highlight.js';
+import 'highlight.js/styles/atom-one-dark.css';
+
 export default function Chat() {
     const [prompt, setPrompt] = useState("");
 	const [isFetching, setIsFetching] = useState(false)
 	const [chatlog, setChatlog] = useState<{ text: string; class: string }[]>([])
 	const promptInput = useRef<HTMLInputElement>(null);
+	
+	async function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
+        setIsFetching(true)
+        
+        let promptToAdd = {
+            text: prompt,
+            class: 'chat-input'
+        }
+        
+        setChatlog(prevChatlog => [...prevChatlog, promptToAdd])
+        
+        //reset input field
+        if (promptInput.current) {
+            promptInput.current.value = "";
+        }
+        
+        try {
+            const res = await fetch("/api/gemini", {
+               	method: "POST",
+               	headers: { "Content-Type": "application/json" },
+               	body: JSON.stringify({ prompt }),
+            });
+            
+            if (!res.ok) {
+               	throw new Error('Failed to submit the data. Please try again.')
+            } else {
+                const data = await res.json()
+                
+                let responseToAdd = {
+                   	text: data.response,
+                   	class: 'chat-output',
+                   	markdown: data.response
+                }
+                
+                setChatlog(prevChatlog => [...prevChatlog, responseToAdd])
+                setIsFetching(false)
+                
+                setTimeout(() => {
+                   	hljs.initHighlighting();
+                }, 1)
+            }
+            
+        } catch (error) {
+            console.error(error)
+        }
+    }
     
     return (
         <div className="kajmilgpt-chat-wrapper">
             <div className="kajmilgpt-chat-container">
-                
+                {chatlog.map(chat => (
+					<div key={chat.text} className={chat.class}>
+					    {chat.text}
+					</div>
+				))}
             </div>
             <div className="kajmilgpt-input-wrapper">
-                <div className="kajmilgpt-input-container">
-                    <input type="text" placeholder="Vad kan KajmilGPT hjälpa till med idag?"/>
+                <form onSubmit={handleSubmit} className="kajmilgpt-input-container">
+                    <input onChange={(e) => setPrompt(e.target.value)} ref={promptInput} type="text" placeholder="Vad kan KajmilGPT hjälpa till med idag?"/>
                     <button className="model-button">gemini-2.5-flash</button>
-                    <button className="submit-button">
+                    <button type="submit" className="submit-button">
                         <ArrowUpIcon size={18} weight={"bold"} color={"white"}/>
                     </button>
-                </div>
+                </form>
             </div>
         </div>
     )
